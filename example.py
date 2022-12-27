198a6de02f2f6858a50bf94492f0da4b36ddb9bd
nachikethmurthy <47597910+nachikethmurthy@users.noreply.github.com>
2022-12-27 07:17:27 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,13 @@
+a1 = 100
+a2 = 200
+
+def add(num1,num2):
+    print(f"num1 is {num1}")
+    print(f"num2 is {num2}")
+    print(f"sum is {num1+num2}")
+
+def subtract(num1,num2):
+    print(f"num1 is {num1}")
+    print(f"num2 is {num2}")
+    print(f"difference is {num1-num2}")
+ 
